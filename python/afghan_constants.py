aa7628d277b41856a8df0de52c49e28021027875
Laksh Bhasin <lbhasin@caltech.edu>
2015-03-23 13:51:58 +0000

    Modified VOTING_FRACTION to 0.5093.
    This is based on using the voting-age population instead of the number
    of registered voters in the country (which is partly inflated by
    diaspora voters and partly inflated by over-registration). Several
    figures and files had to be regenerated as a result of this update.

--- python/afghan_constants.py
+++ python/afghan_constants.py
@@ -2,17 +2,19 @@
 # Description: Useful constants for this election. Import this into other
 # scripts.
 
-# The total number of registered voters in Afghanistan, as well as the
-# total population of Afghanistan. Both of these figures come from
-# http://www.idea.int/vt/countryview.cfm?CountryCode=AF
-NUM_REGISTERED_VOTERS = 20845988.0
+# The total voting age population of Afghanistan, as well as the total
+# population of Afghanistan. Both of these figures come from
+# http://www.idea.int/en/vt/countryview.cfm?CountryCode=AF
+VOTING_AGE_POPULATION = 16208255.0
 AFGHAN_POPULATION = 31822848.0
 
 # We will assume that the fraction of people who are eligible to vote in
 # each district/province is just the ratio of the above two quantities.
 # This homogeneity assumption might not always hold, but it's the best we
-# can do.
-VOTING_FRACTION = NUM_REGISTERED_VOTERS/float(AFGHAN_POPULATION)
+# can do. Also, this is really a *maximum* voting-eligible fraction, based
+# on how many people are of voting age (I'd imagine that prisoners can't
+# vote).
+VOTING_FRACTION = VOTING_AGE_POPULATION/float(AFGHAN_POPULATION)
 
 # Colors to use for the two candidates' plots.
 ABDULLAH_COLOR = "#FFAE19"
